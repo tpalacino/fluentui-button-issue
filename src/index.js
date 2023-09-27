333d920131b4858a7e4c700ba40426491354200d
Troy Palacino <tpalacino@users.noreply.github.com>
2023-09-27 15:49:31 -0400

    Removed the fix for the FluentUI

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./pages";
-import { initializeIcons } from '@fluentui/react'
+// import { initializeIcons } from '@fluentui/react'
+/*
+
+Steps to resolve the issue:
+    1. Uncomment line 3
+
+*/
 
 const container = document.getElementById("app");
 const root = createRoot(container)
